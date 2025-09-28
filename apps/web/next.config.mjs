8ad86617bb710742f4bddb73b6441e909992eb6e
Thomas Carlson <thomasmcarlson@gmail.com>
2025-09-28 16:00:27 -0500

    Revert "Updated next config"
    This reverts commit b61660b1b1dfd177a219c929be6fd74772e15245.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const currentDir = dirname(fileURLToPath(import.meta.url));
-
-export default {
+const nextConfig = {
   async rewrites() {
     return [
       { source: "/api/:path*", destination: "http://localhost:3000/:path*" }, // your Express port
     ];
   },
-  outputFileTracingRoot: currentDir,
-  // ...other config
 };
+
+module.exports = {
+  outputFileTracingRoot: __dirname,
+  // ...o
+};
+
+export default nextConfig;
