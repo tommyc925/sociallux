b61660b1b1dfd177a219c929be6fd74772e15245
Thomas Carlson <thomasmcarlson@gmail.com>
2025-09-27 20:18:38 -0500

    Updated next config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+export default {
   async rewrites() {
     return [
       { source: "/api/:path*", destination: "http://localhost:3000/:path*" }, // your Express port
     ];
   },
+  outputFileTracingRoot: currentDir,
+  // ...other config
 };
-
-module.exports = {
-  outputFileTracingRoot: __dirname,
-  // ...o
-};
-
-export default nextConfig;
