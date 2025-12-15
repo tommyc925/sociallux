6f002d34609b942e255158f4be7c986042e82112
Thomas Carlson <thomasmcarlson@gmail.com>
2025-12-14 23:09:29 -0600

    Adding some comments since I take too long of breaks between working sessions

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import router from "./routes/routes.js";
 import dotenv from "dotenv";
 
+// establish a connection to environment variables to access with process.env
 dotenv.config();
 
+// Set up MongoDB connection for project
 import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI;
@@ -12,11 +14,14 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Initialize Express app
 const app = express();
 app.use(express.json());
 
+// Initiate the initial entry points for the API
 app.use("/api", router);
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,18 @@ import {
   patchAtom,
 } from "../controllers/atomController.js";
 
+// Initialize router
 const router = express.Router();
 
+// Establish a root route
 router.get("/", homeController);
 
+// Atom CRUD routes
 router.post("/atom", createAtom);
 router.get("/atoms", getAtoms);
 router.get("/atom/:id", getAtomById);
 router.delete("/atom/:id", deleteAtom);
 router.patch("/atom/:id", patchAtom);
 
+// Export the router
 export default router;
